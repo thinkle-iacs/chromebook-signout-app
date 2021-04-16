7baf628299ddf89ed8fd779f43ab2c394fec4dcb
Tom Hinkle <tmhinkle@gmail.com>
2021-04-16 15:24:42 -0400

    Fancy up the asset display a bit

--- src/AssetDisplay.svelte
+++ src/AssetDisplay.svelte
@@ -1,38 +1,76 @@
 <script type="ts">
   import type { Asset } from "./inventory";
   export let asset: Asset;
+  export let showOwner: boolean = false;
 </script>
 
 <div class="w3-container row">
   <div class="tag w3-round-xlarge w3-indigo">
     {asset["Asset Tag"]}
   </div>
-  <span class="model">
-    {asset.Make || ""}
-    {asset.Model || ""}
-    {#if asset["Year of Purchase"]}
-      ({asset["Year of Purchase"]})
+  <div class="column">
+    <div class="model limit w3-small">
+      <b
+        >{asset.Make || ""}
+        {asset.Model || ""}</b
+      >
+      {#if asset["Year of Purchase"]}
+        ({asset["Year of Purchase"]})
+      {/if}
+    </div>
+    <div class="limit w3-tiny">
+      {#if asset.Serial}
+        <div class="w3-monospace">
+          s/n: {asset.Serial}
+        </div>
+      {/if}
+      {#if asset["MAC-Wireless"]}
+        <span class="w3-monospace">
+          mac: {asset["MAC-Wireless"]}
+        </span>
+      {/if}
+    </div>
+    {#if showOwner}
+      <div class="w3-small">
+        {#if asset["Email (from Student (Current))"]}
+          Currently signed out to
+          <b>{asset["Email (from Student (Current))"]}</b>
+        {/if}
+        {#if asset["Full Name (from User)"]}
+          Currently signed out to staff
+          <b>{asset["Full Name (from User)"]}</b>
+        {/if}
+      </div>
     {/if}
-  </span>
-  {#if asset.Serial}
-    <span class="w3-monospace">
-      s/n: {asset.Serial}
-    </span>
-  {/if}
-  {#if asset["MAC-Wireless"]}
-    <span class="w3-monospace">
-      mac: {asset["MAC-Wireless"]}
-    </span>
-  {/if}
-  <slot />
+  </div>
 </div>
 
 <style>
+  .limit {
+    max-width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .limit:hover {
+    text-overflow: initial;
+    overflow: initial;
+  }
   .tag {
     display: inline-block;
     padding: 8px;
+    align-self: center;
+    align-items: center;
+    margin-right: 4px;
   }
   .row {
     padding-bottom: 8px;
+    display: flex;
+    flex-direction: row;
+  }
+  .column {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
   }
 </style>
