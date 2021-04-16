50d62e32a7896923b668c19c5a529be4d9117838
Tom Hinkle <tmhinkle@gmail.com>
2021-04-15 23:07:35 -0400

    Add header + filtering

--- src/SignoutHistoryTable.svelte
+++ src/SignoutHistoryTable.svelte
@@ -1,15 +1,42 @@
 <script type="ts">
+  import { flip } from "svelte/animate";
+  import { fade } from "svelte/transition";
   import type { SignoutHistoryEntry } from "./signoutHistory";
   import type { Student } from "./students";
   export let signoutHistoryItems: SignoutHistoryEntry[];
   export let student: Student | null;
+  let currentOnlyMode = false;
+  let studentOnlyMode = false;
   $: console.log(signoutHistoryItems);
 </script>
 
 <table class="w3-table">
-  {#each signoutHistoryItems as item}
+  <tr>
+    <th> Time </th>
+    <th> Asset </th>
+    <th>
+      Student
+      {#if student}
+        <div class="w3-small normal">
+          <input type="checkbox" bind:checked={studentOnlyMode} /> only {student.Name}
+        </div>
+      {/if}
+    </th>
+    <th> Notes </th>
+    <th>
+      Status
+      <div class="w3-small normal">
+        <input type="checkbox" bind:checked={currentOnlyMode} />
+        current only
+      </div>
+    </th>
+  </tr>
+  {#each signoutHistoryItems.filter((i) => (!currentOnlyMode || i["Is Latest Change"]) && (!studentOnlyMode || (i["Email (from Students)"] && i["Email (from Students)"][0] == student.Email))) as item (item.Num)}
     <tr
-      class:match={item["Email (from Students)"] &&
+      in:fade
+      out:fade
+      class:match={student &&
+        item["Email (from Students)"] &&
         item["Email (from Students)"][0] == student.Email}
       class:w3-lime={item.Status == "Returned" && item["Is Latest Change"]}
       class:w3-amber={item.Status == "Out" && item["Is Latest Change"]}
@@ -55,5 +82,9 @@
   .match {
     font-weight: bold;
     color: black;
+  }
+  .normal {
+    font-weight: 200;
+    white-space: nowrap;
   }
 </style>
