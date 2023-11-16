4e1a6b07a77ba7d5a729a10bf023d216a6f403b3
Tom Hinkle <tmhinkle@gmail.com>
2023-11-16 15:36:43 -0500

    T ype fixes

--- src/data/staff.ts
+++ src/data/staff.ts
@@ -1,21 +1,21 @@
-import { writable, get } from 'svelte/store';
+import { writable, get } from "svelte/store";
 
-export type Staff {
-  _id : string
-  Email : string,
-  First : string,
-  Last : string,
-  Role : string,
-  Department : string,
-  psnOID : string,
-  School : string,
-  'Full Name' : string,
-}
+export type Staff = {
+  _id: string;
+  Email: string;
+  First: string;
+  Last: string;
+  Role: string;
+  Department: string;
+  psnOID: string;
+  School: string;
+  "Full Name": string;
+};
 export let staffStore = writable({});
 
 let searchCache = {};
 
-export async function searchForStaff (name) {
+export async function searchForStaff(name) {
   if (searchCache[name]) {
     return searchCache[name];
   } else {
@@ -23,21 +23,19 @@ export async function searchForStaff (name) {
       "/.netlify/functions/index?mode=staff&name=" + encodeURIComponent(name)
     );
     let json = await response.json();
-    console.log('Got staff data',json);
-    const results = []
-    staffStore.update(
-      ($staffStore)=>{
+    console.log("Got staff data", json);
+    const results = [];
+    staffStore.update(($staffStore) => {
       for (let result of json) {
-        const staffObject : Staff = {...result.fields,_id:result.id}
+        const staffObject: Staff = { ...result.fields, _id: result.id };
         results.push(staffObject);
-        $staffStore[staffObject['Full Name']] = staffObject;
+        $staffStore[staffObject["Full Name"]] = staffObject;
         $staffStore[staffObject.Email] = staffObject;
         $staffStore[staffObject.psnOID] = staffObject;
         $staffStore[staffObject._id] = staffObject;
       }
       return $staffStore;
-    }
-    );
+    });
     searchCache[name] = results;
     return results;
   }
