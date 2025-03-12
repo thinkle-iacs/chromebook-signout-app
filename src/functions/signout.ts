7a35afb048bafaa47109141c09a0c8c524b6dc5a
Tom Hinkle <tmhinkle@gmail.com>
2025-03-11 20:41:01 -0400

    linting

--- src/functions/signout.ts
+++ src/functions/signout.ts
@@ -1,34 +1,48 @@
-import type { APIGatewayEvent, Context } from 'aws-lambda'
-import { signoutHistoryBase } from './Airtable'
+import type { APIGatewayEvent, Context } from "aws-lambda";
+import { signoutHistoryBase } from "./Airtable";
 
-export async function handler(
-  event : APIGatewayEvent, context : Context 
-) {
-  const { assetRecordId, staffRecordId, studentRecordId, Notes, Status, FormUser, DailyLoan } = event.queryStringParameters
-  console.log('Got params',JSON.stringify(
-    { assetRecordId, staffRecordId, studentRecordId, Notes, Status, DailyLoan }
-  ))
+export async function handler(event: APIGatewayEvent, context: Context) {
+  const {
+    assetRecordId,
+    staffRecordId,
+    studentRecordId,
+    Notes,
+    Status,
+    FormUser,
+    DailyLoan,
+  } = event.queryStringParameters;
+  console.log(
+    "Got params",
+    JSON.stringify({
+      assetRecordId,
+      staffRecordId,
+      studentRecordId,
+      Notes,
+      Status,
+      DailyLoan,
+    })
+  );
   let fields = {
-    Asset : [assetRecordId],
+    Asset: [assetRecordId],
     Status,
     Notes,
     FormUser,
-    DailyLoan : DailyLoan=='true' && true || false,
-  }
+    DailyLoan: (DailyLoan == "true" && true) || false,
+  };
   if (studentRecordId) {
-    fields.Student = [studentRecordId]
+    fields.Student = [studentRecordId];
   }
   if (staffRecordId) {
-    fields.Staff = [staffRecordId]
+    fields.Staff = [staffRecordId];
   }
-  
+
   let result = await signoutHistoryBase.create([
     {
-      fields 
-      }    
-  ])
+      fields,
+    },
+  ]);
   return {
     statusCode: 200,
-    body: JSON.stringify(result);
+    body: JSON.stringify(result),
   };
 }
