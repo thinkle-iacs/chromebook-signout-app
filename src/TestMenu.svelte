d85f23b326cd574e1c4ab3b5b00eba43c1b5f7f6
Tom Hinkle <tmhinkle@gmail.com>
2025-08-12 15:53:35 -0400

    linting

--- src/TestMenu.svelte
+++ src/TestMenu.svelte
@@ -7,13 +7,17 @@
 
 <div class="w3-container">
   <h3>Test Utilities</h3>
-  <p class="w3-small w3-text-gray">Internal tools not shown in main navigation.</p>
+  <p class="w3-small w3-text-gray">
+    Internal tools not shown in main navigation.
+  </p>
   <ul class="w3-ul w3-card w3-small" style="max-width:480px;">
     <li>
       <a class="w3-text-blue" href="/sis-test/">SIS API Test Tool</a>
     </li>
     <li>
-      <a class="w3-text-blue" href="/schedule-test/">Schedule Structure Tester</a>
+      <a class="w3-text-blue" href="/schedule-test/"
+        >Schedule Structure Tester</a
+      >
     </li>
   </ul>
 </div>
