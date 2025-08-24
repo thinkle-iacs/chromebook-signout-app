02ca5a3af64540d4c8af0e11a6a78edd826f98e1
Tom Hinkle <tmhinkle@gmail.com>
2025-08-24 12:35:20 -0400

    Fix dev hard-coded user

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,23 +1,28 @@
+import { logger } from "@utils/log";
 import { writable, derived } from "svelte/store";
 
-export let user = writable({
-  // DO NOT COMMIT - LOCAL TESTING ONLY
-  /*email: "thinkle@innovationcharter.org",
-  user_metadata: {
-    full_name: "Tom Hinkle",
-  },*/
-  // END NON-COMMITAL SECTION
-});
+export let user = writable(
+  window.location.hostname === "localhost"
+    ? {
+        // DO NOT COMMIT - LOCAL TESTING ONLY
+        email: "thinkle@innovationcharter.org",
+        user_metadata: {
+          full_name: "Tom Hinkle",
+        },
+        // END NON-COMMITAL SECTION
+      }
+    : {}
+);
 export let loggedIn = derived([user], ([user]) => {
   if (user && user.email) {
-    console.log("email is", user.email);
+    logger.logRegular("email is", user.email);
     if (
       user.email.split("@")[0].indexOf(".") == -1 &&
       user.email.split("@")[1] == "innovationcharter.org"
     ) {
       return true;
     } else {
-      console.log("Invalid email", user.email);
+      logger.logError("Invalid email", user.email);
       return false;
     }
   }
